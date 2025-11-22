aaab33e3318caa8be29af04c07e8af7e6094f2eb
Vincent <vincent.mazet@unistra.fr>
2025-11-22 22:02:27 +0100

    TP

--- _static/toto.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'm' || event.key === 'M') {
-        // Sélectionner toutes les balises <mark> sur la page
-        const marks = document.querySelectorAll('mark');
-        marks.forEach(mark => {
-            mark.classList.toggle('active');
-        });
-    }
-});
